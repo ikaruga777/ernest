7459c1498e9035e7692e679c0978890e3ec76ccb
ikaruga <ikaruga777@gmail.com>
2019-07-20 13:38:14 +0900

    差分を文字列として返す

--- lib/emoji_json.rb
+++ lib/emoji_json.rb
@@ -1,11 +1,24 @@
 class EmojiJson
-  def self.diff(prev_json,current_json)
-    prev_set = Set.new prev["emoji"].keys
-    current_set = Set.new current["emoji"].keys
-    puts "prev count#{prev_set.count}"
-    puts "current count #{current_set.count}"
-    diff = current_set - prev_set
-    puts "diff count #{diff.count}"
-    puts ":"+diff.to_a.join(': :')+":"
+
+  def initialize(prev_json, current_json)
+    @prev_json = prev_json
+    @current_json = current_json
+  end
+
+  def diff
+    prev_emojis = Set.new @prev_json["emoji"].keys
+    current_emojis = Set.new @current_json["emoji"].keys
+    diff = current_emojis - prev_emojis
+    format(prev_emojis, current_emojis,diff)
+  end
+
+  private
+
+  def format(prev_emojis, current_emojis,diff)
+    content = "prev count#{prev_emojis.count}\n"
+    content << "current count #{current_emojis.count}\n"
+    content << "diff count #{diff.count}\n"
+    content << ":" + diff.to_a.join(': :') + ":" if diff.count > 0
+    content
   end
 end
