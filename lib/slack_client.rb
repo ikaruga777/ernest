c15c7449fb2d2403b7c682bfd467858049ae77fc
ikaruga <ikaruga777@gmail.com>
2019-07-20 12:53:54 +0900

    slackに通知できるようにする

--- lib/slack_client.rb
+++ lib/slack_client.rb
@@ -1,3 +1,4 @@
+require 'slack-ruby-client'
 class SlackClient
   def initialize(api_token)
     @api_token = api_token
@@ -7,7 +8,20 @@ class SlackClient
     response = Net::HTTP.get(URI.parse "https://slack.com/api/emoji.list?token=#{@api_token}&pretty=1")
   end
 
-  def notify
+  def notify(content, as_user = false)
+    client.chat_postMessage(channel: '#feed', text: content, as_user: as_user)
+  end
 
+  private
+
+  def client
+     client ||= generate_client
+  end
+
+  def generate_client
+     Slack.configure do |config|
+      config.token = @api_token
+    end
+    Slack::Web::Client.new
   end
 end
