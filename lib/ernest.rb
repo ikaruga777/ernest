2d8308e63887b215379dba6b6871ec9fbb9641ec
ikaruga <ikaruga777@gmail.com>
2019-07-20 13:39:49 +0900

    差分をSlackに流す

--- lib/ernest.rb
+++ lib/ernest.rb
@@ -4,20 +4,24 @@ require 'date'
 require 'yaml'
 require 'slack_client'
 require 'gist_client'
+require 'emoji_json'
 
 class Ernest
   def run
     settings = YAML.load_file('settings.yaml')
 
     slack_client = SlackClient.new(settings['slack']['token'])
-    dump = slack_client.dump_emoji_list
+    gist_client = GistClient.new(settings['github']['api_endpoint'], settings['github']['access_token'], settings['github']['save_gist_id'])
 
-    gist_client = Gist.new(settings['github']['api_endpoint'], settings['github']['access_token'], settings['github']['save_gist_id'])
-    last_dump = gist_client.fetch_last
+    emoji_dump = slack_client.dump_emoji_list
 
-    JSON.parse(last_dump)['files']['emoji_list.json']['content']
+    last_emoji_dump = gist_client.fetch_last_content "emoji_list.json"
+    puts emoji_dump
+    puts last_emoji_dump
+    emoji_json = EmojiJson.new(JSON.parse(last_emoji_dump), JSON.parse(emoji_dump))
+    content = emoji_json.diff
+    slack_client.notify(content)
 
-    content = dump
-    gist_client.save("emoji_list.json", content)
+    gist_client.save("emoji_list.json", emoji_dump)
   end
 end
