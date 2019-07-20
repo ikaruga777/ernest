24ba1c8f12a4091b3f22ba574f59d77d8d3560ca
ikaruga <ikaruga777@gmail.com>
2019-07-20 13:41:24 +0900

    fix format

--- lib/ernest.rb
+++ lib/ernest.rb
@@ -14,12 +14,11 @@ class Ernest
     gist_client = GistClient.new(settings['github']['api_endpoint'], settings['github']['access_token'], settings['github']['save_gist_id'])
 
     emoji_dump = slack_client.dump_emoji_list
-
     last_emoji_dump = gist_client.fetch_last_content "emoji_list.json"
-    puts emoji_dump
-    puts last_emoji_dump
+
     emoji_json = EmojiJson.new(JSON.parse(last_emoji_dump), JSON.parse(emoji_dump))
     content = emoji_json.diff
+
     slack_client.notify(content)
 
     gist_client.save("emoji_list.json", emoji_dump)
